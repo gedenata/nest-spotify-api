e6115847195acceeb67119f2bdb8faba94c2ee06
gedenata <igedetirtanata@gmail.com>
2023-09-10 01:31:46 +0800

    feat: add swagger api docs

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import * as cookieParser from 'cookie-parser';
 import * as session from 'express-session';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   dotenv.config();
@@ -17,6 +18,36 @@ async function bootstrap() {
     }) as any,
   );
 
+  const config = new DocumentBuilder()
+    .setTitle('Spotify Web API')
+    .setDescription('Spotify Web API Documentation')
+    .setVersion('1.0.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document, {
+    customSiteTitle: 'Spotify Web API Docs',
+    swaggerOptions: {
+      persistAuthorization: true,
+      filter: true,
+      tagsSorter: 'alpha',
+      operationsSorter: 'alpha',
+      defaultModelsExpandDepth: false,
+    },
+    customCss: `
+      #swagger-ui .topbar {
+        display: none;
+      }
+      .swagger-ui .model-box-control:focus,
+      .swagger-ui .models-control:focus,
+      .swagger-ui .opblock-summary-control:focus {
+        outline: none;
+      }
+    `,
+  });
+
   await app.listen(3000);
 }
+
 bootstrap();
