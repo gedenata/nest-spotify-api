f50c98b1e22ecb2fc11a02075c65ee6cabbe4c34
gedenata <igedetirtanata@gmail.com>
2023-09-11 08:22:38 +0800

    fix: error response

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -64,24 +64,28 @@ export class UserService {
         const { status, data } = error.response;
         if (status === HttpStatus.UNAUTHORIZED) {
           // Unauthorized error (e.g., token expired)
-          throw new HttpException('Unauthorized', HttpStatus.UNAUTHORIZED);
+          throw new HttpException(data, HttpStatus.UNAUTHORIZED);
         } else if (status === HttpStatus.FORBIDDEN) {
           // Forbidden error (e.g., insufficient permissions)
-          throw new HttpException('Forbidden', HttpStatus.FORBIDDEN);
+          throw new HttpException(data, HttpStatus.FORBIDDEN);
         } else if (status === HttpStatus.TOO_MANY_REQUESTS) {
           // Rate limiting error
-          throw new HttpException(
-            'Too Many Requests',
-            HttpStatus.TOO_MANY_REQUESTS,
-          );
+          throw new HttpException(data, HttpStatus.TOO_MANY_REQUESTS);
+        } else if (status === HttpStatus.BAD_REQUEST) {
+          // Bad Request error (customize as needed)
+          throw new HttpException(data, HttpStatus.BAD_REQUEST);
         } else {
           // Handle other errors (customize as needed)
           throw new HttpException(data, status);
         }
       } else {
         // Handle network errors or unexpected issues
+        const errorResponse: ErrorResponseDto = {
+          status: HttpStatus.INTERNAL_SERVER_ERROR,
+          message: 'Internal Server Error',
+        };
         throw new HttpException(
-          'Internal Server Error',
+          errorResponse,
           HttpStatus.INTERNAL_SERVER_ERROR,
         );
       }
@@ -118,24 +122,28 @@ export class UserService {
         const { status, data } = error.response;
         if (status === HttpStatus.UNAUTHORIZED) {
           // Unauthorized error (e.g., token expired)
-          throw new HttpException('Unauthorized', HttpStatus.UNAUTHORIZED);
+          throw new HttpException(data, HttpStatus.UNAUTHORIZED);
         } else if (status === HttpStatus.FORBIDDEN) {
           // Forbidden error (e.g., insufficient permissions)
-          throw new HttpException('Forbidden', HttpStatus.FORBIDDEN);
+          throw new HttpException(data, HttpStatus.FORBIDDEN);
         } else if (status === HttpStatus.TOO_MANY_REQUESTS) {
           // Rate limiting error
-          throw new HttpException(
-            'Too Many Requests',
-            HttpStatus.TOO_MANY_REQUESTS,
-          );
+          throw new HttpException(data, HttpStatus.TOO_MANY_REQUESTS);
+        } else if (status === HttpStatus.BAD_REQUEST) {
+          // Bad Request error (customize as needed)
+          throw new HttpException(data, HttpStatus.BAD_REQUEST);
         } else {
           // Handle other errors (customize as needed)
           throw new HttpException(data, status);
         }
       } else {
         // Handle network errors or unexpected issues
+        const errorResponse: ErrorResponseDto = {
+          status: HttpStatus.INTERNAL_SERVER_ERROR,
+          message: 'Internal Server Error',
+        };
         throw new HttpException(
-          'Internal Server Error',
+          errorResponse,
           HttpStatus.INTERNAL_SERVER_ERROR,
         );
       }
